eb56468aecc44fe73c9e776650efee4534414257
Gabriel Jaime Galeano Montoya <gabrieljgm@gmail.com>
2018-05-15 14:05:13 -0500

    firma y crifrado soap

--- src/main/java/co/com/bancolombia/service/verifyaccountowner/VerifyAccountOwnerApplication.java
+++ src/main/java/co/com/bancolombia/service/verifyaccountowner/VerifyAccountOwnerApplication.java
@@ -13,15 +13,24 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import org.apache.camel.model.dataformat.JaxbDataFormat;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.WebServiceClientException;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+
+import org.springframework.ws.soap.security.support.KeyStoreFactoryBean;
+import org.springframework.ws.soap.security.support.TrustManagersFactoryBean;
+import org.springframework.ws.soap.security.wss4j2.callback.KeyStoreCallbackHandler;
+import org.springframework.ws.soap.security.wss4j2.support.CryptoFactoryBean;
+
 
 @SpringBootApplication
 public class VerifyAccountOwnerApplication {
@@ -33,6 +42,24 @@ public class VerifyAccountOwnerApplication {
 
 @Configuration
 class Config{
+	
+	@Value("${client.ssl.trust-store}")
+	private Resource trustStore;
+
+//	@Value("${client.ssl.trust-store-password}")
+//	private String trustStorePassword;
+	
+	@Value("${client.ssl.key-alias}")
+	private String trustStoreAlias;
+	
+	@Value("${microservice.ssl.key-store}")
+	private Resource keyStore;
+
+//	@Value("${microservice.ssl.key-store-password}")
+//	private String keyStorePassword;
+	
+	@Value("${microservice.ssl.key-alias}")
+	private String keyAlias;
 
 	/**
 	 * JaxbContext for DataPower SOAP http://grupobancolombia.com/intf/Producto/Depositos/ConsultaCuentaDepositos/V1.0
@@ -65,17 +92,82 @@ class Config{
 	 * Spring SOAP Template
 	 * @implNote this bean is used by the Apache Camel Spring-ws component
 	 * @return WebServiceTemplate
+	 * @throws Exception 
 	 */
 	@Bean
-	public WebServiceTemplate webServiceTemplate(){
-		trust();
-		ClientInterceptor[] interceptors = new ClientInterceptor[] {new LogHttpHeaderClientInterceptor()};
+	public WebServiceTemplate webServiceTemplate() throws Exception{
+		//trust();
+		//ClientInterceptor[] interceptors = new ClientInterceptor[] {new LogHttpHeaderClientInterceptor()};
+		ClientInterceptor[] interceptors = {securityInterceptor()};
 		WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
 		webServiceTemplate.setMarshaller(jaxb2Marshaller());
 		webServiceTemplate.setUnmarshaller(jaxb2Marshaller());
 		webServiceTemplate.setInterceptors(interceptors);
+		
+		
+
+		
+		
 		return webServiceTemplate;
 	}
+	
+	@Bean
+	public KeyStoreCallbackHandler securityCallbackHandler() {
+		KeyStoreCallbackHandler callbackHandler = new KeyStoreCallbackHandler();
+		//callbackHandler.setPrivateKeyPassword(trustStorePassword);
+		//callbackHandler.setPrivateKeyPassword(binding.getTrustStorePassword());
+		callbackHandler.setPrivateKeyPassword("desarrollo");
+		return callbackHandler;
+	}
+	
+	@Bean
+    public Wss4jSecurityInterceptor securityInterceptor() throws Exception {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        //binding = BluemixExternalConfig.getInstance().getSecretBinding(cloudantBindingPath, cloudantBindingFile,
+		//		isBase64, CloudantBinding.class);
+        // set security actions
+        securityInterceptor.setSecurementActions("Signature Encrypt");
+
+        // sign the request
+        securityInterceptor.setSecurementUsername(keyAlias);
+        //securityInterceptor.setSecurementPassword(keyStorePassword);
+        //securityInterceptor.setSecurementPassword(binding.getKeyStorePassword());
+        securityInterceptor.setSecurementPassword("desarrollo");
+        securityInterceptor.setSecurementSignatureCrypto(getKeyStoreCryptoFactoryBean().getObject());
+
+        // encrypt the request
+        securityInterceptor.setSecurementEncryptionUser(trustStoreAlias);
+        securityInterceptor.setSecurementEncryptionCrypto(getCryptoFactoryBean().getObject());
+        securityInterceptor.setSecurementSignatureKeyIdentifier("DirectReference");
+        
+     // sign the response
+        securityInterceptor.setValidationActions("Timestamp Signature Encrypt");
+        securityInterceptor.setValidationSignatureCrypto(getCryptoFactoryBean().getObject());
+        securityInterceptor.setValidationDecryptionCrypto(getKeyStoreCryptoFactoryBean().getObject());      
+        securityInterceptor.setValidationCallbackHandler(securityCallbackHandler());
+  
+        return securityInterceptor;
+    }
+	
+    @Bean
+    public CryptoFactoryBean getCryptoFactoryBean() throws IOException {
+        CryptoFactoryBean cryptoFactoryBean = new CryptoFactoryBean();
+        //cryptoFactoryBean.setKeyStorePassword(trustStorePassword);
+        //cryptoFactoryBean.setKeyStorePassword(binding.getTrustStorePassword());
+        cryptoFactoryBean.setKeyStorePassword("desarrollo");
+        cryptoFactoryBean.setKeyStoreLocation(trustStore);
+        return cryptoFactoryBean;
+    }
+	
+	 @Bean
+    public CryptoFactoryBean getKeyStoreCryptoFactoryBean() throws IOException {
+        CryptoFactoryBean cryptoFactoryBean = new CryptoFactoryBean();
+        //cryptoFactoryBean.setKeyStorePassword(keyStorePassword);
+        //cryptoFactoryBean.setKeyStorePassword(binding.getKeyStorePassword());
+        cryptoFactoryBean.setKeyStorePassword("desarrollo");
+        cryptoFactoryBean.setKeyStoreLocation(keyStore);
+        return cryptoFactoryBean;
+    }
 
 	public void trust(){
 		try {
